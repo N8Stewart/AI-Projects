af990ff8240a42dcae38add8cbd5347338166799
N8Stewart <Nate.Stewart12@gmail.com>
2015-09-12 00:00:59 -0400

    Complete the successor puzzle function

--- 8-puzzle/matlab/successor_8puzzle.m
+++ 8-puzzle/matlab/successor_8puzzle.m
@@ -30,5 +30,64 @@ function [ child_states, actions ] = successor_8puzzle( parent_state )
     % The rows in child_states and actions should correspond. That is, each
     % row (state) in child_states should be the state that results from
     % performing the move from the same row in actions
+    
+    % --------------------------------------------------------------------
+    % Find the blank space
+    for i = 1:9
+       if parent_state(i) == 0
+           blank = i;
+           break;
+       end
+    end
+    
+    % Determine the actions possible
+    
+    switch (blank);
+        case 1
+            actions = [ 2 4 ];
+        case 2
+            actions = [ 1 2 4 ];
+        case 3
+            actions = [ 1 4 ];
+        case 4
+            actions = [ 2 3 4 ];
+        case 5
+            actions = [ 1 2 3 4 ];
+        case 6
+            actions = [ 1 3 4 ];
+        case 7
+            actions = [ 2 3 ];
+        case 8
+            actions = [ 1 2 3 ];
+        case 9
+            actions = [ 1 3 ];
+    end
+    
+    % For each action possible, determine the child matrix for that action
+    child_states = zeros(size(actions, 2), 9);
+    for i = 1:size(actions, 2)
+       child_states(i,:) = compute_action(blank, actions(i), parent_state);
+    end
+    
+end
+
+% Compute the child matrix if the action provided were to be computed on
+% the parent state
+function [ child_state ] = compute_action( blank, action, parent_state )
+    child_state = parent_state;
+    switch(action)
+        case 1 % Move blank left
+            child_state(blank) = child_state(blank - 1);
+            child_state(blank - 1) = 0;
+        case 2 % Move blank right
+            child_state(blank) = child_state(blank + 1);
+            child_state(blank + 1) = 0;
+        case 3 % Move blank up
+            child_state(blank) = child_state(blank - 3);
+            child_state(blank - 3) = 0;
+        case 4 % Move blank down
+            child_state(blank) = child_state(blank + 3);
+            child_state(blank + 3) = 0;
+    end
 end
 

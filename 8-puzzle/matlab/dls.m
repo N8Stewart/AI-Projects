2d773df5418532a6307da1bfbac6c4d2f9d0b6ba
N8Stewart <Nate.Stewart12@gmail.com>
2015-09-15 14:30:42 -0400

    Remove pdf from code.

--- 8-puzzle/matlab/dls.m
+++ 8-puzzle/matlab/dls.m
@@ -51,3 +51,42 @@ function [ solution_state, solution_path ] = dls(initial_state, successor_func, 
     
 end
 
+% Same thing as the normal DLS algorithm, except prevent states from
+% oscillating back and forth.
+function [ solution_state, solution_path ] = optimized_dls(initial_state, previous_action, successor_func, goaltest_func, depth_limit )
+    % Set the solution state and path if nothing is found
+    solution_state = [];
+    solution_path = [];
+    
+    % If the initial state is the goal state, return.
+    if (goaltest_func(initial_state))
+        solution_state = initial_state;
+        return;
+    end
+    
+    % while we have not reached the depth limit, recurse down the children
+    if (depth_limit > 0)
+        [child_states, actions] = successor_func(initial_state);
+        % for each possible action
+        for i = 1:length(actions)
+            % Do not recurse if the action is the opposite of the previous
+            successor_action = actions(i);
+            if (successor_action == 1 && previous_action == 2 || ...
+                successor_action == 2 && previous_action == 1 || ...
+                successor_action == 3 && previous_action == 4 || ...
+                successor_action == 4 && previous_action == 3)
+                continue;
+            end
+            [solution_state, solution_path] = ...
+                optimized_dls(child_states(i, :), actions(i), successor_func, goaltest_func, depth_limit - 1);
+            % If a solution_state has been reached, append the action and
+            % return
+            if (~isempty(solution_state))
+                solution_path = [actions(i) solution_path(1:end)];
+                return;
+            end
+        end
+    else
+        return; 
+    end
+end

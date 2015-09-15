257384c427c1970f8ca4047b97d42adfe2921e5e
N8Stewart <Nate.Stewart12@gmail.com>
2015-09-14 21:31:22 -0400

    Complete BFS.

--- 8-puzzle/matlab/bfs.m
+++ 8-puzzle/matlab/bfs.m
@@ -13,5 +13,58 @@ function [ solution_state, solution_path ] = bfs(initial_state, successor_func, 
     %actions) needed to reach that state
     
     %If no solution is found, return the empty matrix []
+    
+    % --------------------------------------------------------------------
+    
+    % Set the solution state and path if nothing is found
+    solution_state = [];
+    solution_path = [];
+    
+    counter = 1; % Set the first search to root node
+    num_states = 1;
+    % Initialize the matrix queue with the initial state
+    state(counter, :) = initial_state;
+    predecessor(counter) = 0;
+    action(counter) = 0;
+    
+    % While there are still paths in the queue to be explored
+    while (counter <= num_states)
+        % Check for the goal state
+        if (goaltest_func(state(counter, :)))
+            solution_state = state(counter, :);
+            % Collect the path into an array
+            current_predecessor = counter;
+            num_predecessors = 0;
+            while(predecessor(current_predecessor) ~= 0)
+                 num_predecessors = num_predecessors + 1;
+                 solution_path(num_predecessors) = action(current_predecessor);
+                 current_predecessor = predecessor(current_predecessor);
+            end
+            solution_path = flip(solution_path, 2); % flip the path
+            break;
+        end
+        
+        % Append the successors of the current state to the end of queue
+        [ child_states, actions ] = successor_func(state(counter, :));
+        for i = 1:size(actions, 2)
+            % Optimization: Prevent previous state from entering queue
+            successor_action = actions(i);
+            previous_action = action(counter);
+            if (successor_action == 1 && previous_action == 2 || ...
+                successor_action == 2 && previous_action == 1 || ...
+                successor_action == 3 && previous_action == 4 || ...
+                successor_action == 4 && previous_action == 3)
+                continue;
+            end
+            
+            num_states = num_states + 1;
+            state(num_states, :) = child_states(i, :);
+            action(num_states) = actions(i);
+            predecessor(num_states) = counter;
+        end
+        
+        counter = counter + 1;
+    end
+    
 end
 

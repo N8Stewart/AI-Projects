bec56355da2461ac7e5eafb28282ba69bbd98ff2
N8Stewart <Nate.Stewart12@gmail.com>
2015-09-15 14:47:12 -0400

    Implement IDS.

--- 8-puzzle/matlab/ids.m
+++ 8-puzzle/matlab/ids.m
@@ -11,6 +11,15 @@ function [ solution_state, solution_path ] = ids(initial_state, successor_func, 
     %If no solution is found, return the empty matrix []
     
     % --------------------------------------------------------------------
-    
+    % Loop from 1 to 31 and call DLS for each depth. 31 is the max number
+    % of moves to solve the 8 puzzle, so if it is not found at depth 31,
+    % the solution does not exist.
+    for i = 1:31
+        [solution_state, solution_path] = dls(initial_state, successor_func, goaltest_func, i);
+        % If the solution_state is not empty, return the solution
+        if (~isempty(solution_state)) 
+            return;
+        end
+    end
 end
 

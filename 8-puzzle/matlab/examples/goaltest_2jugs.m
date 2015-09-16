00ce17094525844c8bb64fa7bb155efdd5d3b391
N8Stewart <Nate.Stewart12@gmail.com>
2015-09-16 14:46:24 -0400

    Removed examples from the directory.

--- 8-puzzle/matlab/examples/goaltest_2jugs.m
+++
@@ -1,5 +0,0 @@
-function [ is_goal ] = goaltest_2jugs( state )
-    % 3gal jug has exactly 1 gal in it
-    is_goal = ( state(1)==1 );
-end
-
--- 8-puzzle/matlab/examples/goaltest_vacuum.m
+++
@@ -1,5 +0,0 @@
-function [ is_goal ] = goaltest_8puzzle( state )
-    %No dirt left
-    is_goal=all(state(2:3)==0);
-end
-
